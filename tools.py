58c0de365cc3c130622ef18ddf0a2c1c28a13a55
Tejahni Desire <td6241@princeton.edu>
2023-07-10 10:59:21 -0400

    test 3

---
+++ tools.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from astropy import units as u
+
+def plot_with_units(x_values, y_values, label=None, xlabel='' ,ylabel=''):
+    plt.plot(x_values, y_values,  label=label)
+    if str(x_values[0].unit) != u.dimensionless_unscaled:
+        plt.xlabel(str(xlabel)+ " (" + str(x_values[0].unit) + ")")
+    else:
+        plt.xlabel(str(xlabel))
+        
+    if str(y_values[0].unit) != u.dimensionless_unscaled:
+        plt.ylabel(str(ylabel)+ " (" + str(y_values[0].unit) + ")")
+    else:
+        plt.ylabel(str(ylabel))
+
+def scatter_with_units(x_values, y_values, label=None, xlabel='' ,ylabel=''):
+    plt.scatter(x_values, y_values,  label=label)
+    if str(x_values[0].unit) != u.dimensionless_unscaled:
+        plt.xlabel(str(xlabel)+ " (" + str(x_values[0].unit) + ")")
+    else:
+        plt.xlabel(str(xlabel))
+        
+    if str(y_values[0].unit) != u.dimensionless_unscaled:
+        plt.ylabel(str(ylabel)+ " (" + str(y_values[0].unit) + ")")
+    else:
+        plt.ylabel(str(ylabel))
+
