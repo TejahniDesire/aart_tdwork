1e8d92d8dc0b73b9f234af11785e0b4ec318a0dc
Tejahni Desire <td6241@princeton.edu>
2024-03-07 12:59:59 -0500

    test

--- EZPaths.py
+++
@@ -1,4 +0,0 @@
-aartPath = '/home/tej/Desktop/Code_Stuff/Repositories/aart'
-aartPathResults = '/home/tej/Desktop/Code_Stuff/Repositories/aart_results/Pixel_Convergence/'
-aartOutput = aartPathResults + 'results/'
-modeldir = aartPath + "/paramModels/"
