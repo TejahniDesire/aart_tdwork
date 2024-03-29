35137b4ab5485e9920642c0a6cc02d3015548ae2
Tejahni Desire <td6241@princeton.edu>
2024-03-29 11:22:00 -0400

    Updates

--- astroPloting.py
+++ astroPloting.py
@@ -472,8 +472,8 @@ def fullImage(fig,ax0,ax1,limit,thin_intensity,thick_intensity,thin_radii,thick_
 
     im0im = np.log10(thin_intensity[3])
     im0im[im0im==-np.Infinity] = 0
-    im0 = ax0.imshow(im0im,vmin=vmin, vmax=vmax, origin="lower", cmap="afmhot", extent=[-limit, limit, -limit, limit]
-                     ,norm=matplotlib.colors.LogNorm())
+    im0 = ax0.imshow(im0im, origin="lower", cmap="afmhot", extent=[-limit, limit, -limit, limit]
+                     ,norm=matplotlib.colors.LogNorm(vmin=vmin,vmax=vmax))
 
     ax0.set_xlim(-10, 10)  # units of M
     ax0.set_ylim(-10, 10)
@@ -485,8 +485,8 @@ def fullImage(fig,ax0,ax1,limit,thin_intensity,thick_intensity,thin_radii,thick_
     # Optically thick
     im1im = np.log10(thick_intensity[3])
     im1im[im1im==-np.Infinity] = 0
-    im1 = ax1.imshow(im1im,vmin=vmin, vmax=vmax, origin="lower", cmap="afmhot",  extent=[-limit, limit, -limit, limit]
-                     ,norm=matplotlib.colors.LogNorm())
+    im1 = ax1.imshow(im1im, origin="lower", cmap="afmhot",  extent=[-limit, limit, -limit, limit]
+                     ,norm=matplotlib.colors.LogNorm(vmin=vmin,vmax=vmax))
 
     #
     ax1.set_xlim(-10, 10)  # units of M
