e1c291449352cc110bab43c39079ec6f69702392
Tejahni Desire <td6241@princeton.edu>
2024-04-05 12:32:45 -0400

    Updates

--- smallRuns.py
+++ smallRuns.py
@@ -57,11 +57,25 @@ import fileloading
 # current_model = 'ModelB13'
 
 # Solo Run 1
-current_run = "soloRun1"
-current_bp = astroModels.bp_soloRun1
-current_var_params = ["p_mag"]
-current_geo_grid_names = ["ModelA","ModelB"]
-current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
+# current_run = "soloRun1"
+# current_bp = astroModels.bp_soloRun1
+# current_var_params = ["p_mag"]
+# current_geo_grid_names = ["ModelA","ModelB"]
+# current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
+# action = {
+#     "var": "nu0",
+#     "start": 670e9,
+#     "stop": 700e9,
+#     "step": 20e9,
+#     "images": True
+# }
+# current_models = ['ModelB1']
+
+current_run = "soloRun2"
+current_bp = astroModels.bp_soloRun2
+current_var_params = []
+current_geo_grid_names = ["ModelB"]
+current_geo_grid_values = [(["a"], [str(.9)])]
 action = {
     "var": "nu0",
     "start": 670e9,
