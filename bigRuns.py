07a1ea65de1a2f22734f8e13744f4d6f6281b117
Tejahni Desire <td6241@princeton.edu>
2024-04-16 00:14:32 -0400

    Updates

--- bigRuns.py
+++ bigRuns.py
@@ -41,20 +41,20 @@ import fileloading
 # }
 
 # Test Run 2
-# current_run = "testRun2"
-# current_bp = astroModels.bp_testRun1
-# current_var_params = ["p_temp","p_mag"]
-# current_geo_grid_names = [`"ModelA", "ModelB"]
-# current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
-# action = {
-#     "var": "nu0",
-#     "start": 3.00e+10,
-#     "stop": 6.00e+10,
-#     "step": 1.00e+10,
-#     "images": True
-# }
-# isNormalized = True
-# blurr_policy = False
+current_run = "testRun2"
+current_bp = astroModels.bp_testRun1
+current_var_params = ["p_temp","p_mag"]
+current_geo_grid_names = ["ModelA", "ModelB"]
+current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
+action = {
+    "var": "nu0",
+    "start": 3.00e+10,
+    "stop": 6.00e+10,
+    "step": 1.00e+10,
+    "images": True
+}
+isNormalized = True
+blurr_policy = True
 # action = {
 #     "var": "nu0",
 #     "start": 670e9,
@@ -98,26 +98,26 @@ import fileloading
 
 #
 # Full Run
-current_run = "run1"
-current_bp = astroModels.bp_run1
-current_var_params = ["p_temp", "p_mag"]
-current_geo_grid_names = ["ModelA", "ModelB"]
-current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
-action = {
-    "var": "nu0",
-    "start": 10e9,
-    "stop": 700e9,
-    "step": 20e9,
-    "images": True
-}
+# current_run = "run1"
+# current_bp = astroModels.bp_run1
+# current_var_params = ["p_temp", "p_mag"]
+# current_geo_grid_names = ["ModelA", "ModelB"]
+# current_geo_grid_values = [(["a"], [str(.3)]),(["a"], [str(.9)])]
+# action = {
+#     "var": "nu0",
+#     "start": 10e9,
+#     "stop": 700e9,
+#     "step": 20e9,
+#     "images": True
+# }
 #     "var": "nu0",
 #     "start": 670e9,
 #     "stop": 700e9,
 #     "step": 20e9,
 #     "images": True
 # }
-isNormalized = True
-blurr_policy = True
+# isNormalized = True
+# blurr_policy = True
 
 #
 # sub_paths, all_intensity_models, total_models_count, run_type, variable_param_ranges, constant_params =\
@@ -133,7 +133,7 @@ blurr_policy = True
 bigRun = classRunComputing.BigRuns(current_run,current_bp,current_var_params,
                                    current_geo_grid_values,current_geo_grid_names)
 # bigRun.createGeoGrid()
-# bigRun.creatIntensityGrid(action,isNormalized,blurr_policy)
+bigRun.creatIntensityGrid(action,isNormalized,blurr_policy)
 bigRun.blurrGraphCreation(action)
 bigRun.graphCreation(action)
 
