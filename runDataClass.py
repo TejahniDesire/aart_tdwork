3c36c6d5858c0508891e2375c20497cb56956fcc
Tejahni Desire <td6241@princeton.edu>
2024-06-07 13:41:39 -0400

    Updates

--- runDataClass.py
+++ runDataClass.py
@@ -105,7 +105,7 @@ exp1 = runData("exp1",
                [(["a"], [str(15 / 16)])],
                ["ModelA"],
                )
-exp1.setisNormalized(True)
+exp1.setisNormalized(False)
 
 # testRun1______________________________________________________________________
 
